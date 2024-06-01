2f932db9a481c9fdd135d685c21445845c20c38d
divyam772 <singhdivyam77@gmail.com>
2024-06-01 14:27:56 +0530

    ant-design-setup with the vite-typscript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "antd";
+import { Typography } from "antd";
+function App() {
+  return (
+    <>
+      <Button>ant design button</Button>
+      <Typography>hlo there</Typography>
+    </>
+  );
+}
+
+export default App;
